c96520645085a4b9e1c5d7be9bfbbfdfe2bf6914
madan <reddymadan0512#gmail.com>
2023-06-08 23:03:08 +0530

    add

--- index.js
+++ index.js
@@ -1,14 +1,60 @@
 function Rock() {
-  let computerNumber = " ";
-
   const randomNumber = Math.random();
-  console.log(randomNumber);
+  let computerMove = "";
   if (randomNumber >= 0 && randomNumber < 1 / 3) {
-    computerNumber = "it is Rock";
+    computerMove = "Rock";
   } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-    computerNumber = "it is Paper";
+    computerMove = "Paper";
   } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-    computerNumber = "it is scissors";
+    computerMove = "scissors";
   }
-  console.log(computerNumber);
+  let result = " ";
+  if (computerMove === "Rock") {
+    result = "Tie";
+  } else if (computerMove === "Paper") {
+    result = "you lose";
+  } else if (computerMove === "Scissors") {
+    result === "you win";
+  }
+  alert(`You picked rock. Computer picked ${computerMove}. ${result}`);
+}
+function Paper() {
+  const randomNumber = Math.random();
+  let computerMove = "";
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    computerMove = "Rock";
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    computerMove = "Paper";
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    computerMove = "scissors";
+  }
+  let result = " ";
+  if (computerMove === "Rock") {
+    result = "you win";
+  } else if (computerMove === "Paper") {
+    result = "Tie";
+  } else if (computerMove === "Scissors") {
+    result === "you win";
+  }
+  alert(`You picked rock. Computer picked ${computerMove}. ${result}`);
+}
+function Scissors() {
+  const randomNumber = Math.random();
+  let computerMove = "";
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    computerMove = "Rock";
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    computerMove = "Paper";
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    computerMove = "scissors";
+  }
+  let result = " ";
+  if (computerMove === "Rock") {
+    result = "you lose";
+  } else if (computerMove === "Paper") {
+    result = "you win";
+  } else if (computerMove === "Scissors") {
+    result === "Tie";
+  }
+  alert(`You picked rock. Computer picked ${computerMove}. ${result}`);
 }
