42fcc6c48834fe86c2d744054f5f3376c1e4e819
madan <reddymadan0512#gmail.com>
2023-06-30 22:13:44 +0530

    add

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+let score = JSON.parse(localStorage.getItem("score")) || {
+  wins: 0,
+  losses: 0,
+  ties: 0,
+};
+
+// let update = (document.getElementById("js-score").innerHTML = ` wins:${score.wins}, losses: ${score.losses}, Ties:${score.ties}`);
+// console.log(update);
+updateEL();
+function updateEL() {
+  let update = (document.getElementById("js-score").innerHTML = `wins:${score.wins}, losses: ${score.losses}, Ties:${score.ties}`);
+  console.log(update);
+}
+
 function Rock() {
   const randomNumber = Math.random();
   let computerMove = "";
@@ -16,7 +30,24 @@ function Rock() {
   } else if (computerMove === "Scissors") {
     result === "you win";
   }
-  alert(`You picked rock. Computer picked ${computerMove}. ${result}`);
+
+  if (result === "you win") {
+    score.wins += 1;
+  } else if (result === "you lose") {
+    score.losses += 1;
+  } else if (result === "Tie") {
+    score.ties += 1;
+  }
+  console.log(result);
+  updateEL();
+
+  document.getElementById("js-result").innerHTML = result;
+  // document.getElementById("js-moves").innerHTML = ` you ${Rock()} - ${computerMove} computer`;
+
+  // document.getElementById("js-score").innerHTML = ` wins:${score.wins}, losses: ${score.losses}, Ties:${score.ties}`;
+
+  // alert(`You picked rock. Computer picked ${computerMove}. ${result}
+  //  wins:${score.wins}, losses: ${score.losses}, Ties:${score.ties} `);
 }
 function Paper() {
   const randomNumber = Math.random();
@@ -34,9 +65,23 @@ function Paper() {
   } else if (computerMove === "Paper") {
     result = "Tie";
   } else if (computerMove === "Scissors") {
-    result === "you win";
+    result === "you lose";
   }
-  alert(`You picked rock. Computer picked ${computerMove}. ${result}`);
+
+  if (result === "you win") {
+    score.wins = score.wins + 1;
+  } else if (result === "you lose") {
+    score.losses += 1;
+  } else if (result === "Tie") {
+    score.ties += 1;
+  }
+
+  // document.getElementById("js-score").innerHTML = ` wins:${score.wins}, losses: ${score.losses}, Ties:${score.ties}`;
+  updateEL();
+  document.getElementById("js-result").innerHTML = result;
+
+  // alert(`You picked rock. Computer picked ${computerMove}. ${result}
+  //   wins:${score.wins}, losses: ${score.losses}, Ties:${score.ties}`);
 }
 function Scissors() {
   const randomNumber = Math.random();
@@ -56,12 +101,102 @@ function Scissors() {
   } else if (computerMove === "Scissors") {
     result === "Tie";
   }
-  alert(`You picked rock. Computer picked ${computerMove}. ${result}`);
+  if (result === "you win") {
+    score.wins = score.wins + 1;
+  } else if (result === "you lose") {
+    score.losses += 1;
+  } else if (result === "Tie") {
+    score.ties += 1;
+  }
+  // document.getElementById("js-score").innerHTML = ` wins:${score.wins}, losses: ${score.losses}, Ties:${score.ties}`;
+  updateEL();
+  document.getElementById("js-result").innerHTML = result;
+
+  localStorage.setItem("score", JSON.stringify(score));
+  // alert(`You picked rock. Computer picked ${computerMove}. ${result}
+  //   wins:${score.wins}, losses: ${score.losses}, Ties${score.ties}`);
+}
+function ResetScore() {
+  score.wins = 0;
+  score.losses = 0;
+  score.ties = 0;
+  updateEL();
+  localStorage.removeItem("score");
+}
+console.log();
+
+function btn() {
+  const btnEl = document.getElementById("btn");
+  console.log(btnEl);
+  if (btnEl.innerHTML === "subscribe") {
+    btnEl.innerHTML = "Subscribed";
+  } else if (btnEl.innerHTML === "subscribed") {
+    btnEl.innerHTML = "Subscribe";
+  } else {
+    btnEl.innerHTML = "subscribe";
+  }
 }
 
-const product = {
-  name: "madan",
-  price: 1999,
-};
-console.log(product);
-console.log(product.name);
+function calTotal() {
+  let cost = document.getElementById("inputEL");
+  let costTotal = Number(cost.value);
+  if (costTotal < 40) {
+    costTotal += 10;
+  }
+  document.getElementById("jsTotalDisplay").innerHTML = `$${costTotal}`;
+}
+document.getElementById("unfree").innerHTML = "order below $40 = $10 charges";
+document.getElementById("free").innerHTML = "order above $40 = freeShipping";
+
+function enter(event) {
+  if (event.Key === "Enter") {
+    calTotal();
+  }
+}
+
+const todoList = [
+  {
+    item: "make dinner",
+    dueDate: "2022-12-22",
+  },
+  {
+    item: "wash dishes",
+    dueDate: "2022-12-22",
+  },
+];
+
+renderTodoList();
+function renderTodoList() {
+  let todoResult = "";
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    // const name = todoObject.name;
+    // const dueDate = todoObject.dueDate;
+    const { item, dueDate } = todoObject;
+
+    const html = `
+    <p>
+    ${item} ${dueDate} 
+    <button onclick="
+    todoList.splice(${i},1);
+    renderTodoList();
+    " >Delete</button>
+    </p>`;
+    todoResult += html;
+  }
+  console.log(todoResult);
+  document.getElementById("displayList").innerHTML = todoResult;
+}
+
+function addList() {
+  let listElements = document.getElementById("AddList");
+  let inputElement = listElements.value;
+  const dataInputElement = document.getElementById("due-date");
+  const dueDate = dataInputElement.value;
+
+  todoList.push({ item, dueDate });
+  console.log(todoList);
+  listElements.value = "  ";
+  renderTodoList();
+}
